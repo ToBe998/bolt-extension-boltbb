95238f273f53953dddcde72b0c1563219fd4e3a3
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-22 17:15:18 +0100

    Add Grunt for CSS generation

---
+++ scss/Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['foundation/bower_components/foundation/scss']
+      },
+      custom: {
+        options: {
+        },
+        files: {
+          '../css/boltbb.custom.css': 'boltbb.custom.scss'
+        }        
+      },
+      custom_min: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          '../css/boltbb.custom.min.css': 'boltbb.custom.scss'
+        }        
+      },
+      dist: {
+        options: {
+        },
+        files: {
+          '../css/boltbb.css': 'boltbb.scss'
+        }        
+      },
+      dist_min: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          '../css/boltbb.min.css': 'boltbb.scss'
+        }        
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      custom: {
+        files: './**/*.scss',
+        tasks: ['sass:custom','sass:custom_min']
+      },
+      dist: {
+          files: './**/*.scss',
+          tasks: ['sass:dist','sass:dist_min']
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('custom', ['sass:custom','sass:custom_min']);
+  grunt.registerTask('default', ['sass:dist','sass:dist_min']);
+}
