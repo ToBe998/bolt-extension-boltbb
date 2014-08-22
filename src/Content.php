343437be966bbe3686afa4fad96046811ae27d1e
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-22 05:56:55 +0100

    Add emppt Content class

---
+++ src/Content.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Content override class
+ * @see http://docs.bolt.cm/extensions#overriding-the-default-8216content8217-class
+ */
+
+namespace Bolt\Extension\BoltBB;
+
+/**
+ * Content override class
+ */
+class Content extends \Bolt\Content
+{
+}
