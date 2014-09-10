d5ea3a84c611f323faf12ebd861c4981cca26c3b
Gawain Lynch <gawain.lynch@gmail.com>
2014-09-10 09:43:02 +0100

    Add CKEditor codemirror
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- js/boltbb.js
+++ js/boltbb.js
@@ -4,14 +4,34 @@
 CKEDITOR.replace( "form[editor]" );
 
 CKEDITOR.editorConfig = function( config ) {
-	config.language = 'en';
+    config.language = 'en';
     config.uiColor = '#DDDDDD';
     config.resize_enabled = false;
+    config.extraPlugins = 'codemirror';
     config.toolbar = [{ name: 'styles',      items: [ 'Format' ] },
                       { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike' ] },
                       { name: 'paragraph',   items: [ 'NumberedList', 'BulletedList', 'Indent', 'Outdent', '-', 'Blockquote' ] },
                       { name: 'links',       items: [ 'Link', 'Unlink' ] },
                       { name: 'source',      items: [ 'Source' ] }];
+
+    config.codemirror = {
+        theme: 'default',
+        lineNumbers: true,
+        lineWrapping: true,
+        matchBrackets: true,
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        enableSearchTools: true,
+        enableCodeFolding: true,
+        enableCodeFormatting: true,
+        autoFormatOnStart: true,
+        autoFormatOnUncomment: true,
+        highlightActiveLine: true,
+        highlightMatches: true,
+        showFormatButton: false,
+        showCommentButton: false,
+        showUncommentButton: false
+    };
 };
 
 /*
