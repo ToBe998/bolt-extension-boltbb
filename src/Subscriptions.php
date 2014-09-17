6b1e896a95f142f422ef367b1d20177272d239a3
Gawain Lynch <gawain.lynch@gmail.com>
2014-09-17 10:50:47 +0100

    PHPDoc for author, copyright & license
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- src/Subscriptions.php
+++ src/Subscriptions.php
@@ -8,7 +8,24 @@ use Bolt\Extension\Bolt\Members\Members;
 /**
  * Subscriptions management for BoltBB
  *
- * @author Gawain Lynch <gawain.lynch@gmail.com>
+ * Copyright (C) 2014  Gawain Lynch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014, Gawain Lynch
+ * @license   http://opensource.org/licenses/GPL-3.0 GNU Public License 3.0
  */
 class Subscriptions
 {
